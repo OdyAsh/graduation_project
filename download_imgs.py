a67fc5fe7a755c3f5cf7ac5b57b0147351db043e
OdyAsh <ashraf196280@bue.edu.eg>
2022-09-18 12:09:22 +0200

    Created functions for fetching google images

--- download_imgs.py
+++ download_imgs.py
@@ -7,7 +7,7 @@ def downloadImgs(imgType, imgsUrls, startIdx = 0, endIdx = -1, offset = 0):
         try:
             if i == endIdx:
                 return
-            img = requests.get(imgsUrls[i]).content
+            img = requests.get(imgsUrls[i], timeout=(10, 30)).content # remove the timeout parameter if you'll leave your computer over night and worry about connection loss (note that you'll probably interrupt the .ipynb because the requests.get() will probably not reuturn at one of the requests, so p.join() in runInParallel() will not finish)
             fileName = f"{imgType[imgType.find('.')+2:]}{format(i+offset, '07d')}.jpg"
             filePath = os.path.join(path, fileName)
             with open(filePath, 'wb') as f:
