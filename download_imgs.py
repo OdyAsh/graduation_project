eb0649a8a0e66bcf71b5b34c654257a59dff0247
OdyAsh <ashraf196280@bue.edu.eg>
2022-09-20 11:02:05 +0200

    cleaned reddit images and updated download_imgs() to convert base64 into binary

--- download_imgs.py
+++ download_imgs.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from base64 import b64decode
 
 def downloadImgs(imgType, imgsUrls, startIdx = 0, endIdx = -1, offset = 0):
     path = os.path.join('dataset/', imgType)
@@ -7,7 +8,11 @@ def downloadImgs(imgType, imgsUrls, startIdx = 0, endIdx = -1, offset = 0):
         try:
             if i == endIdx:
                 return
-            img = requests.get(imgsUrls[i], timeout=(10, 30)).content # remove the timeout parameter if you'll leave your computer over night and worry about connection loss (note that you'll probably interrupt the .ipynb because the requests.get() will probably not reuturn at one of the requests, so p.join() in runInParallel() will not finish)
+            possibleBinaryData = img.split('data:image/jpeg;base64,') # to work with links obtained from google images
+            if len(possibleBinaryData) > 1: # means that the image is stored in base64 format, and the encoding is in the rest of the link, so just decode that part using b64decode() to obtain binary format that could be saved using write(.. 'wb')
+                img = b64decode(possibleBinaryData[1]) # source: https://medium.com/geekculture/scraping-google-image-search-result-dfe01bcbc610
+            else:
+                img = requests.get(imgsUrls[i], timeout=(10, 30)).content # remove the timeout parameter if you'll leave your computer over night and worry about connection loss (note that you'll probably interrupt the .ipynb because the requests.get() will probably not reuturn at one of the requests, so p.join() in runInParallel() will not finish)
             fileName = f"{imgType[imgType.find('.')+2:]}{format(i+offset, '07d')}.jpg"
             filePath = os.path.join(path, fileName)
             with open(filePath, 'wb') as f:
